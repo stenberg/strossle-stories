9b46a54a7e444d15a2e60ec7db6ca8824991918f
Joakim Stenberg <stenberg@users.noreply.github.com>
2019-09-30 12:36:02 +0200

    Update strossle-stories.js

--- strossle-stories.js
+++ strossle-stories.js
@@ -1,3 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
-  console.log('Strossle Stories loaded');
+
+  var posts = document.getElementsByClassName("strossle-post");
+
+  var openStory = function() {
+    alert("Click");
+  };
+
+  for (var i = 0; i < posts.length; i++) {
+    posts[i].addEventListener('click', openStory, false);
+  }
 });
